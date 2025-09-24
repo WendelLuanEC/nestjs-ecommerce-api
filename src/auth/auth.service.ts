7bd31046ee33b7fc5df5632221b8143d35a79704
Wendel Luan <wendelluan@alu.ufc.br>
2025-09-24 10:41:52 -0300

    fix(auth,users): align service return values with unit tests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
-import { MailService } from 'src/mail/mail.service';
+import { MailService } from '../mail/mail.service';
 
 @Injectable()
 export class AuthService {
