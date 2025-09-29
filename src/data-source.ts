5aeeb617f86225779a2d53ea16d332e2fd503550
wendelluan <wendelluan@alu.ufc.br>
2025-09-29 08:26:43 -0300

    fix(datasource) ensure database config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import { User } from './users/user.entity';
-import { Product } from './products/product.entity';
-import { Cart } from './cart/cart.entity';
 import { CartItem } from './cart/cart-item.entity';
+import { Cart } from './cart/cart.entity';
+import { Product } from './products/product.entity';
+import { User } from './users/user.entity';
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'postgres',
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: process.env.DB_USER || 'nestuser',
+  password: process.env.DB_PASSWORD || 'nestpass',
+  database: process.env.DB_NAME || 'nestdb',
   entities: [User, Product, Cart, CartItem],
   migrations: [__dirname + '/migrations/*.{ts,js}'],
 });
